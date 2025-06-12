953d96913fb21087a9c477b2bb7982813417a068
Yusup Hidayat Winata <yusuphidayatwinata@gmail.com>
2025-06-12 10:13:05 +0700

    Update react_agent code and add react_agent_with_memory code

--- Creds.py
+++ Creds.py
@@ -1,23 +1,26 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 class Creds():
     def __init__(self):
-        # Ollama
-        self.ollama_api_key = "ollama"
-        self.ollama_base_url = "http://localhost:11434/v1"
+        # Set .env
+        load_dotenv(dotenv_path=Path(__file__).parent.parent / ".env", override=True)
         
-        # Google
-        self.google_api_key = self._get_goole_api_key()
+        # API Keys
+        self.ollama_api_key = "ollama"
+        self.google_api_key = self._get_env("GOOGLE_API_KEY")
+        self.openai_api_key = self._get_env("OPENAI_API_KEY")
+        self.hugging_face_token = self._get_env("HUGGINGFACE_TOKEN")
+        
+        # Base URL
+        self.ollama_base_url = "http://localhost:11434/v1"
         self.google_base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
         
-        # Hugging Face
-        self.hugging_face_token = self._get_hf_token()
-        
-        print("All API Keys and Tokens are loaded.")
+        print("All API Keys, Tokens, and Base URLs are loaded.")
 
-    def _get_hf_token(self):
-        return os.getenv("HUGGINGFACE_TOKEN", ".env not found.")
-        
-    def _get_goole_api_key(self):
-        return os.getenv("GOOGLE_API_KEY", ".env not found.")
+    def _get_env(self, key: str) -> str:
+        key = os.getenv(key)
+        if not key:
+            raise ValueError("OPENAI_API_KEY not found in environment.")
+        return key
